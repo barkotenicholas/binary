8e0ffa495a8cc6e92e72deb1b582f438f9f2182b
Nicholas Barkote <barkotenicholas@gmail.com>
2022-07-07 15:52:47 +0300

    Added delete methof

--- src/Tree.java
+++ src/Tree.java
@@ -14,6 +14,36 @@ public class Tree {
         if (root != null) root.inorder();
     }
 
+    public void delete(int value){
+        root = delete(root, value);
+    }
+
+    private Node delete(Node subTreeRoot , int value){
+
+        if (subTreeRoot == null){
+            return subTreeRoot;
+        }
+        if(value < subTreeRoot.getData()){
+            subTreeRoot.setLeftNode(delete(subTreeRoot.getLeftNode(),value));
+        } else if (value > subTreeRoot.getData()) {
+            subTreeRoot.setRightNode(delete(subTreeRoot.getRightNode(),value));
+        }else {
+            // case 1 and 2
+            if(subTreeRoot.getLeftNode() == null){
+                return subTreeRoot.getRightNode();
+            } else if (subTreeRoot.getRightNode() == null) {
+                return subTreeRoot.getLeftNode()
+            }
+            //case3
+
+            subTreeRoot.setData(subTreeRoot.getRightNode().min());
+            subTreeRoot.setRightNode(delete(subTreeRoot.getRightNode(), subTreeRoot.getData()));
+        }
+
+        return subTreeRoot;
+
+    }
+
     public Node getRoot(int value){
         if (root != null){
             return root.getNode(value);
